50593bcf2196a597b7a09cb0baf7aa3c7676841f
Bob Nisco <BobNisco@gmail.com>
2013-05-03 21:27:15 -0400

    Frontend niceness

--- hackathon/static/js/main.js
+++ hackathon/static/js/main.js
@@ -1,27 +1,43 @@
 $(document).ready(function(){
-	var data = {
-		labels : ["January","February","March","April","May","June","July"],
-		datasets : [
-			{
-				fillColor : "rgba(220,220,220,0.5)",
-				strokeColor : "rgba(220,220,220,1)",
-				pointColor : "rgba(220,220,220,1)",
-				pointStrokeColor : "#fff",
-				data : [65,59,90,81,56,55,40]
-			},
-			{
-				fillColor : "rgba(151,187,205,0.5)",
-				strokeColor : "rgba(151,187,205,1)",
-				pointColor : "rgba(151,187,205,1)",
-				pointStrokeColor : "#fff",
-				data : [28,48,40,19,96,27,100]
+	var chart = new Highcharts.Chart({
+		chart: {
+			renderTo: 'myChart',
+			type: 'area'
+		},
+		title: {
+			text: 'Hermes'
+		},
+		xAxis: {
+			categories: ["January","February","March","April","May","June","July","August","Septemper","October","November","December"]
+		},
+		yAxis: {
+			title: {
+				text: 'Total Trades'
 			}
-		]
-	}
-	var ctx = document.getElementById("myChart").getContext("2d");
-	var options = {
-
-	};
-	var myNewChart = new Chart(ctx).Line(data, options);
-
+		},
+		series: [{
+			name: 'Web',
+			data : [65,59,90,81,56,55,40,45,69,70,32,10]
+		}, {
+			name: 'Java',
+			data: [28,48,40,19,96,27,100,43,78,88,45,50]
+		}, {
+			name: 'Database',
+			data: [23,65,13,87,12,56,76,23,43,12,76,88]
+		}]
+	});
+	//console.log(chart.series[0].data.);
+	setInterval(function() {
+		var newData1 = [],
+			newData2 = [],
+			newData3 = [];
+		for (var i = 0; i < 12; i++) {
+			newData1[i] = Math.floor(Math.random()*100);
+			newData2[i] = Math.floor(Math.random()*100);
+			newData3[i] = Math.floor(Math.random()*100);
+		}
+		chart.series[0].setData(newData1);
+		chart.series[1].setData(newData2);
+		chart.series[2].setData(newData3);
+	}, 1000);
 });
