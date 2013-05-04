72a0d3e80f22010c9cb0d4aa10b99ece707401f1
Bob Nisco <BobNisco@gmail.com>
2013-05-04 19:25:39 -0400

    JSON turns, yo

--- hackathon/models.py
+++ hackathon/models.py
@@ -1,6 +1,6 @@
 
 from django.db import models
-	
+
 
 class Region(models.Model):
 	REGION_CHOICES = (
@@ -9,10 +9,10 @@ class Region(models.Model):
 		("a", "Asia"),
 	)
 	region = models.CharField(max_length=1, choices=REGION_CHOICES, db_index=True)
-	
+
 	def __unicode__(self):
 		return self.region
-	
+
 class Tier(models.Model):
 	TIER_CHOICES = (
 		("w", "Web"),
@@ -21,10 +21,10 @@ class Tier(models.Model):
 	)
 	tier = models.CharField(max_length=1, choices=TIER_CHOICES, db_index=True)
 	cost = models.IntegerField(db_index=True)
-	
+
 	def __unicode__(self):
 		return self.tier
-	
+
 class Device(models.Model):
 	tier = models.ForeignKey(Tier, db_index=True)
 	region = models.ForeignKey(Region, db_index=True)
@@ -32,24 +32,24 @@ class Device(models.Model):
 
 	def __unicode__(self):
 		return 'Device : ' + str(self.id) + " Region : " + self.region.region + " Count : " + str(self.count) + " Tier : " + self.tier.tier
-	
+
 class Demand(models.Model):
 	region = models.ForeignKey(Region, db_index=True)
 	count = models.IntegerField(db_index=True)
-	
+
 	def __unicode__(self):
 		return self.region.region + ' ' + str(self.count)
-	
+
 
 class Profit(models.Model):
 	last_profit = models.IntegerField(db_index=True)
 	last_potential = models.IntegerField(db_index=True)
 	total_profit = models.IntegerField(db_index=True)
 	total_potential = models.IntegerField(db_index=True)
-	
+
 	def __unicode__(self):
-		return "Profit ID : " + str(self.id) + " last_profit : " + str(self.last_profit) + " last_potential : " + str(self.last_potential) + " total_profit : " + str(self.total_profit) + " total_potential : " + str(self.total_potential) 
-	
+		return "Profit ID : " + str(self.id) + " last_profit : " + str(self.last_profit) + " last_potential : " + str(self.last_potential) + " total_profit : " + str(self.total_profit) + " total_potential : " + str(self.total_potential)
+
 class Turn(models.Model):
 	time = models.DateTimeField(null=True, blank=True, db_index=True)
 	config = models.ManyToManyField(Device, blank=True, related_name='cfg', verbose_name='current distribution')
@@ -58,11 +58,11 @@ class Turn(models.Model):
 	profit = models.ForeignKey(Profit, null=True, blank=True, db_index=True)
 	control = models.ManyToManyField(Device, null=True,blank=True, related_name='control',verbose_name="our move")
 	revenue_cents = models.IntegerField(blank=True, null=True, db_index=True)
-	
-	def __unicode__(self):
-		return 'swag'
-	
 
-	
+	def __unicode__(self):
+		return 'Time: ' + str(self.time)
+
+
+
 if __name__ == "__main__" and __package__ is None:
     __package__ = "hackathon.models"
--- hackathon/views.py
+++ hackathon/views.py
@@ -5,9 +5,9 @@ import datetime
 
 from django.contrib import auth
 from django.conf import settings
-from django.utils import simplejson
+from django.core import serializers
 from django.template import RequestContext
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render_to_response, get_object_or_404
@@ -18,23 +18,23 @@ def index(request):
 	return render_to_response('index.html')
 
 def get_turn_data(request, id=1):
-	
-	turn = Turn.objects.get(id=id)
+
+	turn = Turn.objects.select_related().filter(id=id)
 	if turn:
-		return HttpResponse(json.dumps(response_data), mimetype="application/json")
+		return HttpResponse(serializers.serialize('json', turn), content_type="application/json")
 	else:
-		return 
+		return
 
 
 def test(request):
-	
+
 	run(23493)
-	
+
 	turns = Turn.objects.all()
 	tiers = Tier.objects.all()
 	devices = Device.objects.all()
 	regions = Region.objects.all()
-	
+
 	return render_to_response('index.html', {
 		'turns' : turns,
 		'tiers' : tiers,
