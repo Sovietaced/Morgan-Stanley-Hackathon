be0f9a03932cda2fb6f08d3b455b658fb84d6d18
Sovietaced <Jason.Parraga@bigswitch.com>
2013-05-03 22:56:07 -0400

    draft of models

--- hackathon/models.py
+++ hackathon/models.py
@@ -1,17 +1,17 @@
 from django.db import models
-
-# Create your models here.
-class Device(models.Model):
-	tier = models.ForeignKey(Tier, db_index=True)
-	region = models.ForeignKey(Region, db_index=True)	
-	count = models.IntegerField(db_index=True)
-	createdAt = models.DateTimeField(auto_now=True)
-	updatedAt = models.DateTimeField(auto_now=True)
-
-	def __unicode__(self):
-		return self.id
 	
 
+class Region(models.Model):
+	REGION_CHOICES = (
+		("n", "North America"),
+		("e", "Europe"),
+		("a", "Asia"),
+	)
+	region = models.CharField(max_length=1, choices=REGION_CHOICES, db_index=True)
+	
+	def __unicode__(self):
+		return self.region
+	
 class Tier(models.Model):
 	TIER_CHOICES = (
 		("l", "Laptop"),
@@ -21,28 +21,38 @@ class Tier(models.Model):
 	tier = models.CharField(max_length=1, choices=TIER_CHOICES, db_index=True)
 	cost = models.IntegerField(db_index=True)
 	
-class Region(models.Model):
-	REGION_CHOICES = (
-		("n", "North America"),
-		("e", "Europe"),
-		("a", "Asia"),
-	)
-	region = models.CharField(max_length=1, choices=REGION_CHOICES, db_index=True)
+	def __unicode__(self):
+		return self.tier
 	
-class Decision(models.Model):
-	devices = models.ManyToManyField(Device, verbose_name="list of devices")
-	demands = models.ManyToManyField(Demand, verbose_name="list of demands")
-	
+class Device(models.Model):
+	tier = models.ForeignKey(Tier, db_index=True)
+	region = models.ForeignKey(Region, db_index=True)
+	count = models.IntegerField(db_index=True)
+
+	def __unicode__(self):
+		return self.id
 	
 class Demand(models.Model):
 	region = models.ForeignKey(Region, db_index=True)
 	count = models.IntegerField(db_index=True)
 	
-class Distribution(models.Model):
-	devices = models.ManyToManyField(Device, verbose_name="list of devices")
+	def __unicode__(self):
+		return self.region + ' ' + str(self.count)
+	
 
 class Profit(models.Model):
+	last_profit = models.IntegerField(db_index=True)
+	last_potential = models.IntegerField(db_index=True)
+	total_profit = models.IntegerField(db_index=True)
+	potential = models.IntegerField(db_index=True)
 	
+class Turn(models.Model):
+	config = models.ManyToManyField(Device, related_name='config', verbose_name='current distribution')
+	demands = models.ManyToManyField(Demand, verbose_name="list of demands")
+	distribution = models.ManyToManyField(Device, related_name='distribution', verbose_name="distribution of the last turn")
+	profit = models.ForeignKey(Profit, db_index=True)
+	control = models.ManyToManyField(Device, related_name='control',verbose_name="our move")
+	revenue_cents = models.IntegerField(db_index=True)
 
 	
 
