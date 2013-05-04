f338e1f468e34726de7c90b2ca46c9987102f04a
Bob Nisco <BobNisco@gmail.com>
2013-05-03 22:30:37 -0400

    Test to see if Heroku allows port access

--- hackathon/models.py
+++ hackathon/models.py
@@ -1,55 +1,55 @@
-from django.db import models
+# from django.db import models
 
-# Create your models here.
-class Device(models.Model):
-	tier = models.ForeignKey(Tier, db_index=True)
-	region = models.ForeignKey(Region, db_index=True)	
-	count = models.IntegerField(db_index=True)
-	createdAt = models.DateTimeField(auto_now=True)
-	updatedAt = models.DateTimeField(auto_now=True)
+# # Create your models here.
+# class Device(models.Model):
+# 	tier = models.ForeignKey(Tier, db_index=True)
+# 	region = models.ForeignKey(Region, db_index=True)
+# 	count = models.IntegerField(db_index=True)
+# 	createdAt = models.DateTimeField(auto_now=True)
+# 	updatedAt = models.DateTimeField(auto_now=True)
 
-	def __unicode__(self):
-		return self.id
-	
-
-class Tier(models.Model):
-	TIER_CHOICES = (
-		("l", "Laptop"),
-		("d", "Desktop"),
-		("m", "Mobile"),
-	)
-	tier = models.CharField(max_length=1, choices=TIER_CHOICES, db_index=True)
-	cost = models.IntegerField(db_index=True)
-	
-class Region(models.Model):
-	REGION_CHOICES = (
-		("n", "North America"),
-		("e", "Europe"),
-		("a", "Asia"),
-	)
-	region = models.CharField(max_length=1, choices=REGION_CHOICES, db_index=True)
-	
-class Decision(models.Model):
-	devices = models.ManyToManyField(Device, verbose_name="list of devices")
-	demands = models.ManyToManyField(Demand, verbose_name="list of demands")
-	
-	
-class Demand(models.Model):
-	region = models.ForeignKey(Region, db_index=True)
-	count = models.IntegerField(db_index=True)
-	
-class Distribution(models.Model):
-	devices = models.ManyToManyField(Device, verbose_name="list of devices")
-
-class Profit(models.Model):
-	
-
-	
-
-	
+# 	def __unicode__(self):
+# 		return self.id
 
 
-	
+# class Tier(models.Model):
+# 	TIER_CHOICES = (
+# 		("l", "Laptop"),
+# 		("d", "Desktop"),
+# 		("m", "Mobile"),
+# 	)
+# 	tier = models.CharField(max_length=1, choices=TIER_CHOICES, db_index=True)
+# 	cost = models.IntegerField(db_index=True)
 
-	
+# class Region(models.Model):
+# 	REGION_CHOICES = (
+# 		("n", "North America"),
+# 		("e", "Europe"),
+# 		("a", "Asia"),
+# 	)
+# 	region = models.CharField(max_length=1, choices=REGION_CHOICES, db_index=True)
+
+# class Decision(models.Model):
+# 	devices = models.ManyToManyField(Device, verbose_name="list of devices")
+# 	demands = models.ManyToManyField(Demand, verbose_name="list of demands")
+
+
+# class Demand(models.Model):
+# 	region = models.ForeignKey(Region, db_index=True)
+# 	count = models.IntegerField(db_index=True)
+
+# class Distribution(models.Model):
+# 	devices = models.ManyToManyField(Device, verbose_name="list of devices")
+
+# class Profit(models.Model):
+
+
+
+
+
+
+
+
+
+
 
--- hackathon/views.py
+++ hackathon/views.py
@@ -1,4 +1,6 @@
 # Create your views here.
+import sys
+import socket
 import datetime
 
 from django.contrib import auth
@@ -12,3 +14,26 @@ from django.shortcuts import render_to_response, get_object_or_404
 
 def index(request):
 	return render_to_response('index.html')
+
+def socket_test(request):
+	myPort = 57012
+	connection = connect(myPort)
+	if connection:
+		costs = connection.recv(4096)
+		str = costs
+		connection.send('START')
+		str += " " + connection.recv(4096)
+		return render_to_response('test.html', {
+			'str' : str,
+		})
+	else:
+		print 'Connection Failed'
+
+
+def connect(port):
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect(('67.202.15.69', int(port)))
+	s.send('INIT brogrammers')
+	s.recv(4096)
+	s.send('RECD')
+	return s
